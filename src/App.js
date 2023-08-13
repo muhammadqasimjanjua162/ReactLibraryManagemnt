9e408da5d750a7d8f8af4064744de341b92b65e8
muhammadqasimjanjua162 <qasim.official2@gmail.com>
2023-08-13 23:03:03 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+// import DisplayFormData from "./components/displayFormData/displayFormData";
+import DisplayData from "./components/displayFormData/DisplayData";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <DisplayData />;
 }
 
 export default App;
